38a617f7699b993f2b8a2376e630c39705e7e6dc
Nathan Reilly <nreilly@nreilly.com>
2021-06-27 16:32:08 +1000

    Initial import from https://www.curseforge.com/wow/addons/dyslectic-loot-priority

---
+++ loot_prio_hook.lua
@@ -0,0 +1,40 @@
+BINDING_HEADER_DKP = "Dyslectic_Horse_Priority";
+
+
+
+GameTooltip:HookScript("OnTooltipSetItem", function(tooltip, ...)
+  local itemname, itemlink = tooltip:GetItem()
+  
+  if itemlink then
+
+	
+	priority = search_for_item4(itemlink:match("item:(%d+):"))
+  
+  end
+
+ 
+	
+  
+  if priority then
+  	prio = string.format(" %s", priority)
+  	tooltip:AddLine(prio, 255, 255, 0, true)
+  
+end
+end)
+
+ItemRefTooltip:HookScript("OnTooltipSetItem", function(tooltip, ...)local itemname, itemlink = tooltip:GetItem()
+  
+  if itemlink then
+
+	priority = search_for_item4(itemlink:match("item:(%d+):"))
+
+ 
+
+  end
+  
+  if priority then
+  	prio = string.format(" %s", priority)
+  	tooltip:AddLine(prio, 255, 255, 0, true)
+	
+  end
+  end)
